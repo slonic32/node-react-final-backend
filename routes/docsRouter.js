22a2093f83493625aa33859bb54337dd1239e1d5
Artem Hordiyevskyy <artem2500@gmail.com>
2024-05-07 22:56:16 +0300

    fix

--- routes/docsRouter.js
+++ routes/docsRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "../docs/swagger.json";
+import swaggerDocument from "../docs/swagger.json" assert { type: "json" };
 
 const docsRouter = express.Router();
 
 docsRouter.use("/", swaggerUi.serve);
 docsRouter.get("/", swaggerUi.setup(swaggerDocument));
+
+export default docsRouter;
